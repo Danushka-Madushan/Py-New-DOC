66bbafe89568acb6192bc39dcf855194bf4b3df1
Danushka-Madushan  Official <69332746+Danushka-Madushan@users.noreply.github.com>
2021-01-01 16:37:20 +0530

    Python Script

--- New.py
+++ New.py
@@ -1,12 +1,20 @@
 import sys
 
 try:
-	X = sys.argv[1]
+	Y = sys.argv[2 or 3 or 4 or 5 or 6 or 7 or 8 or 9]
+	if Y:
+		print("\n$ Usage : New.py {FileName.Extension}")
+		sys.exit()
+
 except Exception:
-	print("\n$ Usage : New.py {FileName.Extension}")
-	sys.exit()
-	
-NEW_DOC = open(X, 'w')
-NEW_DOC.close()
-print(f"\n$ Created Document : [ {X} ]")
-sys.exit()
+	try:
+		X = sys.argv[1]
+		if X:
+			NEW_DOC = open(X, 'w')
+			NEW_DOC.close()
+			print(f"\n$ Created Document : [ {X} ]")
+			sys.exit()
+
+	except Exception:
+		print("\n$ Usage : New.py {FileName.Extension}")
+		sys.exit()
